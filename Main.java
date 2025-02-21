7dac0ae76a6b0bef8cfadd1ed386357c0dd0151f
goodoker <evgeny.grigorevskiy@mail.ru>
2025-02-21 21:14:54 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how GIGA IDE suggests fixing it.
+        System.out.println("Evgeny Grigorevskiy");
+
+        int income = 44000;
+        int spending = 30000;
+
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
